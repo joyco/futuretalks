cff71dcf68f5eb037a3101d35d977885654f0fd0
rashmi <rashmiphadte@gmail.com>
2020-03-14 20:52:48 +0530

    add service file

---
+++ final/assets/js/service.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+    var apiUrl = "https://api.themoviedb.org/3/discover/movie";
+    var apiKey = "3dc10d04a30f7b13a6d697435f363644";
+
+    // on load get all the movies 
+    $.ajax({
+        url: apiUrl + "?api_key=" + apiKey + "&sort_by=release_date.desc" + "&page=1",
+        contentType: "application/json",
+        dataType: 'json',
+        success: function (result) {
+            console.log(result);
+        }
+    })
+
+   //search movies  
+   // Note: searchMovies ---> search button id,  searchByName ---> search textbox id(delete this comment once integreated )
+    $('#searchMovies').click(function () {
+        var keyword = $('#searchByName').val()
+        $.ajax({
+            url: apiUrl + "?api_key=" + apiKey + "&keywords=" + keyword + "&page=1",
+            contentType: "application/json",
+            dataType: 'json',
+            success: function (result) {
+                console.log(result);
+            }
+        })
+    });
+})
+
+
+
