0b070788265efffd84405ebbd8985cc5600f11ec
rashmi <rashmiphadte@gmail.com>
2020-03-16 00:23:24 +0530

    add route

--- final/app.js
+++ final/app.js
@@ -3,11 +3,13 @@ var app = angular.module('movieApp',["ngRoute",'lazy-scroll']);
 app.config(function ($routeProvider, $httpProvider) {
     $routeProvider
     .when('/', {
-        templateUrl: 'view/home.html',
+        templateUrl: 'home.html',
         controller: 'movieController'
     })
     .when('/detail', {
-        templateUrl: 'details.html',
-        controller: 'movieController'
-    })
+        templateUrl: '/details.html',
+        // controller: 'movieController'
+    }).otherwise({
+        redirectTo: "/"
+    });
 })
